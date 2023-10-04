c8a8afc2e58577fc1eaa1d210b161be3f52cce4f
Miguel Rodrigues <migb.rodrigues@gmail.com>
2023-10-04 16:37:08 +0200

    Updated Scanner simple test to reflect changes from d718fff

--- app/src/test/java/org/vut/ifje/project/scanner/ScannerTest.java
+++ app/src/test/java/org/vut/ifje/project/scanner/ScannerTest.java
@@ -9,23 +9,23 @@ import java.util.List;
 public class ScannerTest {
     @Test void scanTest() {
         String mock = "add(5, mul(3, sub(10, pow(6, 4))))";
-
+        
         List<Token> expected = Arrays.asList(
                 new Token(TokenType.ADD, "add"),
                 new Token(TokenType.LEFT_PARENTHESIS, "("),
-                new Token(TokenType.NUMBER, "5"),
+                new Token(TokenType.POSITIVE_NUMBER, "5"),
                 new Token(TokenType.COMMA, ","),
                 new Token(TokenType.MUL, "mul"),
                 new Token(TokenType.LEFT_PARENTHESIS, "("),
                 new Token(TokenType.SUB, "sub"),
                 new Token(TokenType.LEFT_PARENTHESIS, "("),
-                new Token(TokenType.NUMBER, "10"),
+                new Token(TokenType.POSITIVE_NUMBER, "10"),
                 new Token(TokenType.COMMA, ","),
                 new Token(TokenType.POW, "pow"),
                 new Token(TokenType.LEFT_PARENTHESIS, "("),
-                new Token(TokenType.NUMBER, "6"),
+                new Token(TokenType.POSITIVE_NUMBER, "6"),
                 new Token(TokenType.COMMA, ","),
-                new Token(TokenType.NUMBER, "4"),
+                new Token(TokenType.POSITIVE_NUMBER, "4"),
                 new Token(TokenType.RIGHT_PARENTHESIS, ")"),
                 new Token(TokenType.RIGHT_PARENTHESIS, ")"),
                 new Token(TokenType.RIGHT_PARENTHESIS, ")"),
