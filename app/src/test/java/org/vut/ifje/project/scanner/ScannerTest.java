f7a4be52e9fe80efec1e765693c2d1f7c1943a7b
Miguel Rodrigues <migb.rodrigues@gmail.com>
2023-10-04 13:29:29 +0200

    Added simple Scanner test

---
+++ app/src/test/java/org/vut/ifje/project/scanner/ScannerTest.java
@@ -0,0 +1,41 @@
+package org.vut.ifje.project.scanner;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ScannerTest {
+    @Test void scanTest() {
+        String mock = "add(5, mul(3, sub(10, pow(6, 4))))";
+
+        List<Token> expected = Arrays.asList(
+                new Token(TokenType.ADD, "add"),
+                new Token(TokenType.LEFT_PARENTHESIS, "("),
+                new Token(TokenType.NUMBER, "5"),
+                new Token(TokenType.COMMA, ","),
+                new Token(TokenType.MUL, "mul"),
+                new Token(TokenType.LEFT_PARENTHESIS, "("),
+                new Token(TokenType.SUB, "sub"),
+                new Token(TokenType.LEFT_PARENTHESIS, "("),
+                new Token(TokenType.NUMBER, "10"),
+                new Token(TokenType.COMMA, ","),
+                new Token(TokenType.POW, "pow"),
+                new Token(TokenType.LEFT_PARENTHESIS, "("),
+                new Token(TokenType.NUMBER, "6"),
+                new Token(TokenType.COMMA, ","),
+                new Token(TokenType.NUMBER, "4"),
+                new Token(TokenType.RIGHT_PARENTHESIS, ")"),
+                new Token(TokenType.RIGHT_PARENTHESIS, ")"),
+                new Token(TokenType.RIGHT_PARENTHESIS, ")"),
+                new Token(TokenType.RIGHT_PARENTHESIS, ")"),
+                new Token(TokenType.EOF, "")
+        );
+
+        Scanner scanner = new Scanner(mock);
+        List<Token> actual = scanner.scan();
+
+        assertIterableEquals(expected, actual);
+    }
+}
