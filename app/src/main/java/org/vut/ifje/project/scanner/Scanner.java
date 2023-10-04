c2baac55246dc6381bbf918d746e4e9558e87282
Miguel Rodrigues <migb.rodrigues@gmail.com>
2023-10-04 16:26:20 +0200

    Added Scanner support for ')', '(', ',' and EOF

---
+++ app/src/main/java/org/vut/ifje/project/scanner/Scanner.java
@@ -0,0 +1,41 @@
+package org.vut.ifje.project.scanner;
+
+
+import java.text.StringCharacterIterator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scanner {
+    private final StringCharacterIterator iterator;
+    private final List<Token> tokens = new ArrayList<>();
+
+    public Scanner(String source) {
+        this.iterator = new StringCharacterIterator(source);
+    }
+
+    public List<Token> scan() {
+        while (!done()) {
+            scanToken();
+        }
+
+        tokens.add(new Token(TokenType.EOF, ""));
+        return tokens;
+    }
+
+    private boolean done() {
+        return iterator.current() == StringCharacterIterator.DONE;
+    }
+
+    private void scanToken() {
+        switch (iterator.current()) {
+            case '(' -> tokens.add(new Token(TokenType.LEFT_PARENTHESIS, "("));
+            case ')' -> tokens.add(new Token(TokenType.RIGHT_PARENTHESIS, ")"));
+            case ',' -> tokens.add(new Token(TokenType.COMMA, ","));
+            default -> {}
+        }
+
+        iterator.next();
+    }
+
+
+}
